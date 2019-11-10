35b59841f17d25d0e814b38462833c521c323251
Kris Haughey <37224839+krishaughey@users.noreply.github.com>
2019-11-09 23:01:27 -0800

    First Push

---
+++ USER-AccountAudit.ps1
@@ -0,0 +1,10 @@
+## Get USERS from select OU (you will be prompted for the OU you wish to search)
+##  and export info to CSV w/ TIMESTAMP and human-friendly lastlogon date
+$timestamp = Get-Date -Format s | ForEach-Object { $_ -replace ":", "." }
+$DN = 'OU='
+$input = Read-Host -Prompt 'enter the OU you wish to search'
+$OU = -join($DN,$input)
+
+Import-Module ActiveDirectory
+
+Get-ADUser -Filter * -SearchBase "$OU,dc=rlcorp,dc=local” -properties Name, samAccountName, Enabled, AccountExpirationDate,lastLogon, WhenChanged, employeeNumber, extensionAttribute6, extensionAttribute9, Office, Country | Select-Object Name, samAccountName, Enabled, AccountExpirationDate, @{n='LastLogon';e={[DateTime]::FromFileTime($_.LastLogon)}}, WhenChanged, employeeNumber, extensionAttribute6, extensionAttribute9, Office, Country | export-csv c:\Temp\$OU-Users-$timestamp.csv
