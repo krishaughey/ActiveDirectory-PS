5b6b713396575119444568fe3c0fb37139b8ae71
Kris Haughey <37224839+krishaughey@users.noreply.github.com>
2019-11-18 16:36:28 -0800

    SelfUpdateManaged - output logging added

--- WSUS-SelfUpdateManaged.ps1
+++ WSUS-SelfUpdateManaged.ps1
@@ -1,7 +1,9 @@
 $ErrorActionPreference= 'silentlycontinue'
-$list = (Get-Content C:\Temp\ServerList.txt)
+$list = (Get-Content C:\Temp\wsusServerList.txt)
 
 Foreach ($server in $list)
 {
  psexec \\$server wuauclt.exe \selfupdatemanaged
 }
+
+$LogOutFile | Out-File C:\Temp\wsusUpdateCheck.csv
