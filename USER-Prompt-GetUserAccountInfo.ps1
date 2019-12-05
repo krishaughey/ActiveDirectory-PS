cfed3e8bb71899da12c2c72246d6f63c4edc2ae2
Kris Haughey <37224839+krishaughey@users.noreply.github.com>
2019-12-04 16:28:27 -0800

    updated scripts for UAR

--- USER-Prompt-GetUserAccountInfo.ps1
+++ USER-Prompt-GetUserAccountInfo.ps1
@@ -1,14 +1,10 @@
-﻿#Prompts for a sAMAccountName to get-ADuser attributes in the current domain
-
+#Prompts for a sAMAccountName to get-ADuser attributes in the current domain
 write-host "enter the sAMAccountName of the user and press ENTER (e.g.- khaughey)" -ForegroundColor Cyan
 
 $input = Read-Host -Prompt "--->"
 $User = $input
 
-Get-ADuser -Identity $input -properties * | select displayname, userPrincipalName , DistinguishedName, Enabled, AccountExpirationDate | Export-csv c:\Temp\$input.csv
+Get-ADuser -Identity $input -properties displayname,userPrincipalName,Enabled,AccountExpirationDate,LastLogon,WhenChanged | select displayname,userPrincipalName,Enabled,AccountExpirationDate,@{n='LastLogon';e={[DateTime]::FromFileTime($_.LastLogon)}},WhenChanged | Export-csv c:\Temp\$input.csv
 
 write-host "**COMPLETE**" -ForegroundColor Yellow
 Write-host "RESULTS have been saved to ---> c:\Temp\$input.csv" -ForegroundColor Cyan
-
-Write-Host -NoNewLine 'Press any key to EXIT...' -ForegroundColor White;
-$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
--- USER-Prompt-UserAccountReview.ps1
+++ USER-Prompt-UserAccountReview.ps1
@@ -1,5 +1,4 @@
-## Get USERS from select OU (you will be prompted for the OU you wish to search)
-##  and export info to CSV w/ TIMESTAMP and human-friendly lastlogon date
+## Get USERS from select OU (you will be prompted for the OU you wish to search) and export info to CSV w/ TIMESTAMP and human-friendly lastlogon date
 $timestamp = Get-Date -Format s | ForEach-Object { $_ -replace ":", "." }
 $DN = 'OU='
 $input = Read-Host -Prompt 'enter the OU you wish to search'
@@ -7,4 +6,5 @@ $OU = -join($DN,$input)
 
 Import-Module ActiveDirectory
 
-Get-ADUser -Filter * -SearchBase "$OU,dc=rlcorp,dc=local” -properties Name, samAccountName, Enabled, AccountExpirationDate,lastLogon, WhenChanged, employeeNumber, extensionAttribute6, extensionAttribute9, Office, Country | Select-Object Name, samAccountName, Enabled, AccountExpirationDate, @{n='LastLogon';e={[DateTime]::FromFileTime($_.LastLogon)}}, WhenChanged, employeeNumber, extensionAttribute6, extensionAttribute9, Office, Country | export-csv c:\Temp\$OU-Users-$timestamp.csv
+Get-ADUser -Filter * -SearchBase "$OU,dc=rlcorp,dc=local” -Server padm075.rlcorp.local -properties Name, samAccountName, Enabled, AccountExpirationDate,lastLogon, WhenChanged, employeeNumber, extensionAttribute6, extensionAttribute9, Office, Country | Select-Object Name,samAccountName,Enabled,AccountExpirationDate,@{n='LastLogon';e={[DateTime]::FromFileTime($_.LastLogon)}},WhenChanged,employeeNumber,extensionAttribute6,extensionAttribute9,Office,Country | export-csv c:\Temp\$OU-Users-$timestamp.csv
+write-host "Process Complete. Check the export directory for your results." -ForegroundColor Green
