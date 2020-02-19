a5b9f8417ad95f95bace506b64e756c838f4fcb8
Kris Haughey <37224839+krishaughey@users.noreply.github.com>
2020-02-19 14:02:45 -0800

    Removed domain-specific info

--- DNS-AddSecondaryZoneTransfer.ps1
+++ DNS-AddSecondaryZoneTransfer.ps1
@@ -2,5 +2,5 @@ $Secondary = "C:\TEMP\Secondary.csv"
 $SecList = Import-Csv $Secondary
 Foreach ($Server in $SecList)
 {
-Set-DnsServerPrimaryZone -ComputerName pwdc074.rlcorp.local -Name “rlcorp.local” -SecondaryServers $SecList.IP –SecureSecondaries TransferToSecureServers
+Set-DnsServerPrimaryZone -ComputerName HostName.Domain -Name “Domain” -SecondaryServers $SecList.IP –SecureSecondaries TransferToSecureServers
 }
