396498699f2fa60b962395d456012e62b65bb212
Kris Haughey <37224839+krishaughey@users.noreply.github.com>
2020-04-29 16:03:24 -0700

    filtered search down to user objects

--- USER-GridView-UserAccountReview.ps1
+++ USER-GridView-UserAccountReview.ps1
@@ -3,13 +3,13 @@
 ### author: Kristopher F. Haughey
 
 $timestamp = Get-Date -Format s | ForEach-Object { $_ -replace ":", "." }
-$OU = Get-ADOrganizationalUnit -Filter * | Out-GridView -PassThru | Out-GridView -PassThru | Select Name,DistinguishedName
+$OU = Get-ADOrganizationalUnit -Filter * | Where-Object {!$_.PSIsContainer -and  ($_.Name -like "*USER*") } | Out-GridView -PassThru | Out-GridView -PassThru | Select Name,DistinguishedName
 $ExportPath = "c:\temp\"
 
-$FileName = "$($OU.Name)-$timestamp.csv"
+$FileName = "$($OU.DistinguishedName)-$timestamp.csv"
 
 Import-Module ActiveDirectory
 
-Get-ADUser -Filter * -SearchBase $OU.DistinguishedName -properties Name, samAccountName, Enabled, AccountExpirationDate,lastLogon, WhenChanged, employeeNumber, extensionAttribute6, extensionAttribute9, Office, Country | Select-Object Name,samAccountName,Enabled,AccountExpirationDate,@{n='LastLogon';e={[DateTime]::FromFileTime($_.LastLogon)}},WhenChanged,employeeNumber,extensionAttribute6,extensionAttribute9,Office,Country | export-csv "$ExportPath$FileName"
+Get-ADUser -Filter * -SearchBase $OU.DistinguishedName -properties Name, samAccountName, Description,Enabled, AccountExpirationDate,lastLogon, WhenChanged,Office | Select-Object Name,samAccountName,Description,Enabled,AccountExpirationDate,@{n='LastLogon';e={[DateTime]::FromFileTime($_.LastLogon)}},WhenChanged,Office | export-csv "$ExportPath$FileName"
 write-host "Process Complete" -ForegroundColor Green
 write-host "Results= $ExportPath$timestamp.csv" -ForegroundColor Cyan
