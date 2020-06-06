d9d82f38a33fe7d48327e3d34bb65d69a27e8084
Kris Haughey <37224839+krishaughey@users.noreply.github.com>
2020-06-05 20:34:32 -0700

    add import-module

--- GPO-Prompt-GPUpdate.ps1
+++ GPO-Prompt-GPUpdate.ps1
@@ -1,13 +1,14 @@
 ## GPUpdate on OU
 ##### Force a group policy update on an OU of machines (random delay of 10 minutes)
 ##### author: Kristopher F. Haughey
-
 #$ErrorActionPreference= 'silentlycontinue'
+Import-Module ActiveDirectory
+Import-Module GroupPolicy
 Write-Host "Enter the searchbase (e.g. <DC=CONTOSO,DC=COM>)" -ForegroundColor Green
 $SearchBase = Read-Host -Prompt "-->"
-$ServerList = get-adcomputer -Filter * -SearchBase $SearchBase | Select-Object Name,dnshostname
+$HostList = get-adcomputer -Filter * -SearchBase $SearchBase | Select-Object Name,dnshostname
 
-Foreach ($hostname in $list)
+Foreach ($Name in $HostList)
 {
-    Invoke-GPUpdate $hostname -Force -AsJob -RandomDelayInMinutes 10
+    Invoke-GPUpdate $Name -Force -AsJob -RandomDelayInMinutes 10
 }
