1ccb828ed38c095b92d8e8fae9d5421fcafb9cc1
Kris Haughey <37224839+krishaughey@users.noreply.github.com>
2020-06-09 20:45:52 -0700

    Tasks still broken

--- COMPUTER-GetScheduledTaskAccounts.ps1
+++ COMPUTER-GetScheduledTaskAccounts.ps1
@@ -1,29 +1,49 @@
 ## Get Scheduled Tasks with Domain StartName
 ##### Get Scheduled Tasks with Domain StartName. Running and not disabled - Not LocalSystem,NT AUTHORITY,$Null
 ##### author: Kristopher F. Haughey
-$timestamp = Get-Date -Format s | ForEach-Object { $_ -replace ":", "." }
-Write-Host "Enter the searchbase (e.g. <DC=CONTOSO,DC=COM>)" -ForegroundColor Green
-$SearchBase = Read-Host -Prompt "-->"
-$ServerList = Get-Adcomputer -Filter 'operatingsystem -like "*server*" -and enabled -eq "true"' -SearchBase $SearchBase | Select-Object Name,DNSHostName
-
-Write-Host "Collecting Scheduled Tasks information..." -ForegroundColor Green
-
-$Array = @()
-foreach ($Server in $ServerList){
-$colItems =(
-    Invoke-Command -ComputerName $Server -ScriptBlock -Wait { $objSchTaskService = New-Object -ComObject Schedule.Service } +
-    Invoke-Command -ComputerName $Server -ScriptBlock -Wait { $objSchTaskService.connect('localhost') } +
-    Invoke-Command -ComputerName $Server -ScriptBlock -Wait { $RootFolder = $objSchTaskService.GetFolder("\") } +
-    Invoke-Command -ComputerName $Server -ScriptBlock -Wait { $ScheduledTasks = $RootFolder.GetTasks(0) } +
-    Invoke-Command -ComputerName $Server -ScriptBlock -Wait { $ScheduledTasks | Select-Object Name, LastRunTime, NextRunTime,@{Name="RunAs";Expression={[xml]$xml = $_.xml ; $xml.Task.Principals.principal.userID }} })
+$ErrorActionPreference = 'silentlycontinue'
+#Create the log
+$TimeStamp = Get-Date -Format s | ForEach-Object { $_ -replace ":", "." }
+Function LogWrite
+{
+Param ([string]$logstring)
+Add-content $Logfile -value $logstring
 }
-  foreach ($Task in $colItems){
-    $Array += New-Object PsObject -Property ([ordered]@{
-        'Server' = $Server
-        'Name' = $Task.Name
-        'LastRunTime' = $Task.LastRunTime
-        'NextRunTime' = $Task.NextRunTime
-        'RunAs' = $Task.RunAs}) #<--- Not sure if this will work
+$LogPath = Read-Host -Prompt "Enter a folder path for the log file >>"
+$LogFile = "$LogPath\ScheduledTasks_$timestamp.log"
+LogWrite "Script Start = $TimeStamp"
+
+#Get ComputerObject Names from OU (prompted)
+$SearchBase = Read-Host -Prompt "Enter the searchbase (e.g. <DC=CONTOSO,DC=COM>) >>"
+LogWrite "SearchBase = $Searchbase"
+$ServerList = Get-Adcomputer -Filter 'operatingsystem -like "*server*" -and enabled -eq "true"' -SearchBase $SearchBase | Select-Object Name
+#$ServerCount = ($ServerList).count
+$ServerNames = $Serverlist.Name
+#LogWrite "Collecting Scheduled Task information from $ServerCount computers..."
+
+#Collect the information from remote machines
+$Array = @()
+foreach ($Server in $ServerNames) {
+  if ( ([string]::IsNullOrEmpty($Server)) ) {
+    LogWrite "$Server not found"
   }
-  $Array | Export-Csv c:\Temp\Services-DomainAccounts_$timestamp.csv -NoTypeInformation
-  Write-Host "export = c:\Temp\Services-DomainAccounts_$timestamp.csv" -ForegroundColor Cyan
+  else {
+    Invoke-Command -ComputerName $Server -ScriptBlock { $objSchTaskService = New-Object -ComObject Schedule.Service }
+    Invoke-Command -ComputerName $Server -ScriptBlock { $objSchTaskService.connect('localhost') }
+    Invoke-Command -ComputerName $Server -ScriptBlock { $RootFolder = $objSchTaskService.GetFolder("\") }
+    Invoke-Command -ComputerName $Server -ScriptBlock { $ScheduledTasks = $RootFolder.GetTasks(0) }
+    Invoke-Command -ComputerName $Server -ScriptBlock { $ScheduledTasks | Select-Object Name, LastRunTime, NextRunTime,@{Name="RunAs";Expression={[xml]$xml = $_.xml ; $xml.Task.Principals.principal.userID}} }
+#Create a temporary table object for appending data
+    foreach ($Task in $colItems){
+      $Array += New-Object PsObject -Property ([ordered]@{
+          'Server' = $Server
+          'Name' = $Task.Name
+          'LastRunTime' = $Task.LastRunTime
+          'NextRunTime' = $Task.NextRunTime
+          'RunAs' = $Task.RunAs})
+    }
+  }
+}
+$Array | Export-Csv c:\Temp\ScheduledTasks_$timestamp.csv -NoTypeInformation
+LogWrite "export = c:\Temp\ScheduledTasks_$timestamp.csv"
+LogWrite "Script End = $TimeStamp"
