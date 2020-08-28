c27e61853097b04afd4088d3c38f84c6ebc354bd
Kris Haughey <37224839+krishaughey@users.noreply.github.com>
2020-08-28 15:05:25 -0700

    working(pre-array)

--- DFS-GetDfsFolderPath.ps1
+++ DFS-GetDfsFolderPath.ps1
@@ -1,7 +1,10 @@
+#Get TargetPath of All DFS Namespaces
+##### author: Kristopher F. Haughey
 
-$Roots = Get-DfsnRoot | Select-Object path
+$GetRoots = Get-DfsnRoot | Select-Object path
+$Roots = $GetRoots.path | foreach-object {$_ -replace ".com",""}
 $Folders = Foreach($Root in $Roots){
-    Get-DfsnFolder -path $Root.path\* | Select-Object path
+    Get-DfsnFolder -path $Root\* | Select-Object path
 }
 $TargetPaths = Foreach($Folder in $Folders){
     Get-DfsnFolderTarget -path $Folder.path | Select-Object TargetPath
