50f26011d79ba67d333f16fa81bc320c073a63b0
Kris Haughey <37224839+krishaughey@users.noreply.github.com>
2021-04-17 19:56:54 -0500

    working

--- WSUS-List-PendingReboot.ps1
+++ WSUS-List-PendingReboot.ps1
@@ -2,24 +2,17 @@
 ##### Get WinServers pending reboot from a list
 ##### REQUIRES PSGallery Module "PendingReboot" https://github.com/bcwilhite/PendingReboot/
 ##### author: Kristopher F. Haughey
-
+Import-Module PendingReboot
 $timestamp = Get-Date -Format s | ForEach-Object { $_ -replace ":", "." }
-$ServerList = Get-Content -Path "c:\Temp\ServerList.txt"
-
+$ServerList = Get-Content c:\Temp\ServerList.txt
 $Array = @()
-foreach ($Server in $ServerList)
-$testConnection = Test-Connection $Server
-If (($testConnection -ne "") -or ($testconnection -ne $null){
-$colItems = Test-PendingReboot -ComputerName $Server.dnshostname -SkipConfigurationManagerClientCheck -SkipPendingFileRenameOperationsCheck -Detailed | Select-Object ComputerName,IsRebootPending,WindowsUpdateAutoUpdate
+foreach ($Server in $ServerList) {
+$colItems = Test-PendingReboot -ComputerName $Server -SkipConfigurationManagerClientCheck -SkipPendingFileRenameOperationsCheck -Detailed | Select-Object ComputerName,IsRebootPending,WindowsUpdateAutoUpdate
   foreach ($RebootCheck in $colItems){
     $Array += New-Object PsObject -Property ([ordered]@{
-      'ServerName' = $RebootCheck.ComputerName
+      'ServerName' = $Server
       'RebootPending' = $RebootCheck.IsRebootPending
       'FromUpdate' = $RebootCheck.WindowsUpdateAutoUpdate})
   }
 }
-Else {
-  Write-Host "WARNING: $Server unreachable"
-}
 $Array | export-csv "C:\Temp\PendingReboot_$timestamp.csv" -NoTypeInformation
-#Write-Host $Array | format-table
