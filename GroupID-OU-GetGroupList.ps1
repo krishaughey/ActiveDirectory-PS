0b2881194a35630f2de6224b1bea1053a6a69a37
krishaughey <37224839+krishaughey@users.noreply.github.com>
2021-06-30 14:10:48 -0500

    changed filter and updated export location

--- GroupID-OU-GetGroupList.ps1
+++ GroupID-OU-GetGroupList.ps1
@@ -2,7 +2,7 @@
 ## uses attribute 'name' for Center by removing the title via REGEX (line9)
 ##### author: Kristopher F. Haughey
 Import-Module ActiveDirectory
-$GroupIDList = Get-ADGroup -Filter " name -ne 'GroupID' " -SearchBase "OU=GroupID,OU=Offices,DC=CARD,DC=COM" -properties Name,Mail | Sort-Object Name | Select-Object Name,Mail
+$GroupIDList = Get-ADGroup -Filter " name -like 'US *' -or name -like '*CARD all*' " -SearchBase "DC=CARD,DC=COM" -properties Name,Mail | Sort-Object Name | Select-Object Name,Mail
 
 $Array = @()
 foreach ($Group in $GroupIDList){
@@ -12,4 +12,4 @@ foreach ($Group in $GroupIDList){
     'Name' = $Group.Name
     'Email' = $Group.Mail} )
 }
-$Array | Export-Csv D:\SFTP\GroupIDExport.csv -NoTypeInformation
+$Array | Export-Csv C:\Temp\GroupIDExport.csv -NoTypeInformation
