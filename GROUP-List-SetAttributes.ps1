f37efc2e2ac4221a4f9ebea9455454e9010cd7d8
Kris Haughey <37224839+krishaughey@users.noreply.github.com>
2022-10-28 09:01:54 -0700

    sanitize

--- GROUP-List-SetAttributes.ps1
+++ GROUP-List-SetAttributes.ps1
@@ -3,4 +3,4 @@ Foreach ($Object in $ImportObject){
    $u = Get-ADGroup $Object."name" -properties name,mail,mailNickName
    $u | Set-ADGroup -Replace @{mail = "$($Object."mail")"}
 }
-Get-ADGroup -filter * -SearchBase "OU=GroupID,OU=Offices,DC=Card,DC=Com" -properties name,mail,mailNickName | Select-Object name,mail,mailNickName | Export-Csv "c:\Temp\GroupID_SMTP_results.csv"
+Get-ADGroup -filter * -SearchBase "OU=GroupID,OU=Offices,DC=<DOMAIN>,DC=Com" -properties name,mail,mailNickName | Select-Object name,mail,mailNickName | Export-Csv "c:\Temp\GroupID_SMTP_results.csv"
