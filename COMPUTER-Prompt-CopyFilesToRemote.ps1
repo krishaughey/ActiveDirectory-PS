9237b5bf81d704eaa586e5d9b1471d6f90a272dc
Kris Haughey <37224839+krishaughey@users.noreply.github.com>
2023-06-26 13:55:05 -0700

    end \

--- COMPUTER-Prompt-CopyFilesToRemote.ps1
+++ COMPUTER-Prompt-CopyFilesToRemote.ps1
@@ -6,9 +6,8 @@ $HostString =  Read-Host -Prompt "-->"
 $ServerList = (Get-ADComputer -Filter {name -like "$HostString"}).Name
 Write-Host "Enter the full path to the file to be copied (e.g. - C:\Temp\File.dll)" -ForegroundColor Green
 $FilePath =  Read-Host -Prompt "-->"
-Write-Host "Enter the destination path, without the hostname (e.g. - d$\Folder\bin)" -ForegroundColor Green
+Write-Host "Enter the destination path, without the hostname (e.g. - d$\Folder\bin\)" -ForegroundColor Green
 $Destination = Read-Host -Prompt "-->"
-
 
 foreach ($Server in $ServerList) {
     Copy-Item $File -Destination "\\$Server\$Destination" -Verbose
