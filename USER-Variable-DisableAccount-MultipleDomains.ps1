b04574853ccb17b844e78cade2190724d60d311c
Kris Haughey <37224839+krishaughey@users.noreply.github.com>
2023-09-15 12:03:32 -0700

    multiple domain user disable

---
+++ USER-Variable-DisableAccount-MultipleDomains.ps1
@@ -0,0 +1,16 @@
+﻿#Disable User Account in Multiple Domains in Trust
+## Get-ADUser in local domain and trusts, based on filter 'like' string, and disable. *With exemption for one domain line 4 col 42
+##### author: Kristopher F. Haughey
+$localDom = (Get-AdDomain).DNSRoot
+$trusts = Get-ADTrust -Filter {name -ne "<EXCEPTION DOMAIN>"} | Select Name
+$userAccount = "<*USER*STRING*>"
+$userLocal = Get-ADUser -Filter {name -like $userAccount} -Server $localDom
+$userLocal | Disable-ADAccount
+
+$userTrusts = foreach($domain in $trusts) {
+    Get-ADUser -Filter {name -like $userAccount} -Server $domain.Name | Disable-ADAccount
+    Get-ADUser -Filter {name -like $userAccount} -Server $domain.Name
+    }
+Get-ADUser -Filter {name -like $userAccount} -Server $localDom
+$userTrusts
+Write-Host "Completed!" -ForegroundColor Cyan
