57dbc2c01509c96f4204ba4ee24c6b48fe959f1c
Kris Haughey <37224839+krishaughey@users.noreply.github.com>
2023-11-08 11:04:01 -0800

    user must change password script

---
+++ USER-List-SetUser-MustChangePassword.ps1
@@ -0,0 +1,12 @@
+#Set User Attribute from list of users
+$timestamp = Get-Date -Format s | ForEach-Object { $_ -replace ":", "." }
+Import-Module ActiveDirectory
+
+$domain = "<DOMAIN>"
+
+$UserList = Get-Content C:\Temp\UserList.txt
+
+foreach($user in $UserList){
+	Set-ADUser -Identity $User -Server $domain -ChangePasswordAtLogon:$true
+}
+Write-Host "Process Complete $timestamp" -foregroundcolor cyan
