1a0c4afeb7cf33491408286b161df25be97ac97d
Kris Haughey <37224839+krishaughey@users.noreply.github.com>
2024-01-25 11:15:06 -0800

    format and description update

--- USER-SelectDomain-AllUsers-Get-Attribute.ps1
+++ USER-SelectDomain-AllUsers-Get-Attribute.ps1
@@ -1,15 +1,15 @@
-﻿### Get all user accounts and report the selected attribute(s)
+﻿### Get all user accounts and report a chosen attribute(s)
 ## THEN export info to CSV w/ TIMESTAMP and human-friendly lastlogon date
 ### author: Kristopher F. Haughey
 Import-Module ActiveDirectory
 
 #Set variables and format TimeStamp
-$AdDomain = "arbonnewest.com"
+$AdDomain = "<DOMAIN>"
 $timestamp = Get-Date -Format s | ForEach-Object { $_ -replace ":", "." }
 $ExportPath = "c:\temp\"
 $FileName = "PreferredLanguage-$AdDomain-$timestamp.csv"
 
 #Get the user accounts (root searcbase), select properties, and export
-Get-ADUser -Filter * -Server $AdDomain -properties preferredLanguage | Select-Object samAccountName,preferredLanguage | export-csv "$ExportPath$FileName" -NoTypeInformation
-write-host "Process Complete" -ForegroundColor Green
-write-host "Results= $ExportPath$timestamp.csv" -ForegroundColor Cyan
+Get-ADUser -Filter * -Server $AdDomain -properties preferredLanguage | Select-Object samAccountName,preferredLanguage | Export-Csv "$ExportPath$FileName" -NoTypeInformation
+Write-Host "Process Complete" -ForegroundColor Green
+Write-Host "Results= PreferredLanguage-$AdDomain-$timestamp.csv" -ForegroundColor Cyan 
