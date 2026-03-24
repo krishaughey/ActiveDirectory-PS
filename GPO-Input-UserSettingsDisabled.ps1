467ac01d5aaa988e1b28dbda3d80cfbea223c503
krishaughey@icloud.com <krishaughey@icloud.com>
2026-03-24 16:46:05 -0700

    corrected desctription and rem'd single target server

--- GPO-Input-UserSettingsDisabled.ps1
+++ GPO-Input-UserSettingsDisabled.ps1
@@ -1,13 +1,13 @@
-## Set GPO status
-##### Set status of a single GPO (AllSettingsEnabled, UserSettingsDisabled, ComputerSettingsDisabled)
+## Get GPO status
+##### Get status of a single GPO (AllSettingsEnabled, UserSettingsDisabled, ComputerSettingsDisabled)
 ##### author: Kristopher F. Haughey
 
 # $timestamp = Get-Date -Format s | ForEach-Object { $_ -replace ":", "." }
 Write-Host "Enter the name of the GPO" -ForegroundColor Green
 $GpoName = Read-Host -Prompt "-->"
-#Write-Host "Enter the DC" -ForegroundColor Green
+Write-Host "Enter the DC" -ForegroundColor Green
 #$DC = Read-Host -Prompt "-->"
-$GpoObject = Get-Gpo -Name "$GpoName" -Server DC2 #$DC
+$GpoObject = Get-Gpo -Name "$GpoName" #-Server $DC
 $GpoStatus = $GpoObject.GpoStatus
 
 write-host $GpoObject.GpoStatus
