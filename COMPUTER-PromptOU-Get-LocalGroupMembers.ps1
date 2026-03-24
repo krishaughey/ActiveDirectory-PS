6338e2d55785fcd89e4c0cc1b675980cbccf896a
krishaughey@icloud.com <krishaughey@icloud.com>
2026-03-24 11:25:13 -0700

    simplify script and pass attributes to invoke

--- COMPUTER-PromptOU-Get-LocalGroupMembers.ps1
+++ COMPUTER-PromptOU-Get-LocalGroupMembers.ps1
@@ -1,24 +1,13 @@
 # Get Local Group Membership
 ## Get the Local Group Membership for a prompted Group against a prompted OU of Servers
 ##### author: Kristopher F. Haughey
+
 $timestamp = Get-Date -Format s | ForEach-Object { $_ -replace ":", "." }
 Import-Module ActiveDirectory
 
-$DomainController = Get-ADDomainController| Select-Object Name
-$SearchBase = Read-Host "Enter searchbase (e.g.- OU=Servers,DC=Domain,DC=local)"
-$ServerList = Get-ADComputer -Filter 'operatingsystem -like "*server*" -and enabled -eq "true"' -SearchBase $SearchBase | Select-Object Name,dnshostname
-
-$Array = @()
-foreach($ServerObject in $ServerList){
-$colItems = Invoke-Command -ComputerName $ServerObject.Name -scriptblock {Get-LocalGroupMember -Group "Administrators"} | Select-Object PSComputerName,Name,ObjectClass,PrincipalSource
-
-    foreach ($Server in $colItems){
-        $Array += New-Object PsObject -Property ([ordered]@{
-            'Server' = $ServerObject.PSComputerName
-            'Group' = $ServerObject.Name
-            'Type' = $ServerObject.ObjectClass
-            'Location' = $ServerObject.PrincipalSource})
-      }
-    }
-$Array | export-csv C:\Temp\Group_Membership_Report_$timestamp.csv -NoTypeInformation
-Write-Host "Export available at C:\Temp\Group_Membership_Report_$timestamp.csv" -ForegroundColor Cyan
+$servers = Get-ADComputer -Filter 'operatingsystem -like "*server*" -and enabled -eq "true"' | Select-Object -ExpandProperty DNSHostName
+$results = foreach ($server in $servers) {
+    Invoke-Command -ComputerName $server -ScriptBlock {Get-LocalGroupMember -Group "Administrators" | Select-Object @{Name = "ComputerName"; Expression = { $using:server } }, Name,ObjectClass,PrincipalSource}
+}
+$results | Export-Csv -Path C:\temp\LocalAdminsReport_$timestamp.csv -NoTypeInformation
+Write-Host "Export available at C:\temp\LocalAdminsReport_$timestamp.csv" -ForegroundColor Cyan
